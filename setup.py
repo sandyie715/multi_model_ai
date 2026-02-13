12f86c902f49d4314751bcea18ff80b159be1ac1
sandyie715 <sanjay715@infomaticscorp.com>
2026-02-13 18:44:19 +0530

    Logging , exception and setup.py codes are done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="multi_model_ai",
+    version="0.0.0",
+    author="sandyie",
+    author_email="business@sandyie.in",
+    packages=find_packages()
+)
